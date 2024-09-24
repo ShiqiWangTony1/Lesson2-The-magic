a07f33e03a8e1860024217f9bee3dba7b17853a6
ShiqiWangTony1 <shiqi.wang0728@gmail.com>
2024-09-24 00:56:48 -0400

    Lesson 2 Magic

---
+++ Lesson2_Magic.py
@@ -0,0 +1,56 @@
+# Task 1: Code Correction
+# number = input("Enter a number: ")
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number = 0:
+#     print("The number is zero.")
+# else number < 0:
+#     print("The number is negative.")
+
+## Answer
+# number = float(input("Enter a number: "))
+
+# if number > 0:
+#     print("The number is positive.")
+# elif number == 0:
+#     print("The number is zero.")
+# else:
+#     print("The number is negative.")
+
+
+# Task 1: Identify the Greatest
+
+# num1 = float(input("Please enter the first number: "))
+# num2 = float(input("Please enter the second number: "))
+# num3 = float(input("Please enter the third number: "))
+
+# largest_num = max(num1,num2,num3)
+# print(f"The largest number is {largest_num}")
+
+# Task 2: Identify the Smallest
+
+# num1 = float(input("Please enter the first number: "))
+# num2 = float(input("Please enter the second number: "))
+# num3 = float(input("Please enter the third number: "))
+
+# smallest_num = min(num1,num2,num3)
+# print(f"The smallest number is {smallest_num}")
+
+# Task 1: Leap Year Checker
+
+# def is_leap_year(year):
+#     if year % 4 == 0 :
+#         return True
+#     else:
+#         return False
+
+# # Input from the user
+# year = int(input("Enter a year: "))
+
+# # Check if the year is a leap year
+# if is_leap_year(year):
+#     print(f"{year} is a leap year.")
+# else:
+#     print(f"{year} is not a leap year.")
+
